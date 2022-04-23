0013e609b9bf3727edc7ddf8a0020ab7ba990e33
FryTery <1329379130@qq.com>
2022-04-23 15:25:43 +0800

    second commit

--- src/main/java/com/fry/git/GitTest.java
+++ src/main/java/com/fry/git/GitTest.java
@@ -4,5 +4,8 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Hello GIT!");
+
+        int i = 0;
+        System.out.println("second commit!");
     }
 }
